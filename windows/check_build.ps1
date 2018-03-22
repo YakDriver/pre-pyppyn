27685a18c3f2e6a23c9c97ff51f3352b27920577
Dirk Avery <dirk.avery@gmail.com>
2018-03-22 13:03:01 -0400

    bugger

--- windows/check_build.ps1
+++ windows/check_build.ps1
@@ -66,7 +66,7 @@ If ($UserdataStatus[0] -eq 0)
             ##Write-S3Object -BucketName "pyppyn" -Folder "C:\git\pyppyn\pyinstaller\dist" -KeyPrefix "20180321/1853_269fd71c5b96" -SearchPattern *.exe
             #Write-Host "9"
             $f = new-object System.IO.FileStream C:\Temp\test.dat, Create, ReadWrite
-            $f.SetLength(22MB)
+            $f.SetLength(12MB)
             $f.Close()
             Write-Host (Get-Date -UFormat "%Y/%m/%d %T")
             Write-S3Object -BucketName $BucketName -Folder "C:\Temp" -KeyPrefix $KeyPrefix -SearchPattern "*.dat"
