3197debd26c7b383c9120c41a940704688e28b51
Dirk Avery <dirk.avery@gmail.com>
2018-03-22 16:27:17 -0400

    wut

--- windows/check_build.ps1
+++ windows/check_build.ps1
@@ -65,15 +65,15 @@ If ($UserdataStatus[0] -eq 0)
             #Write-Host "8"
             ##Write-S3Object -BucketName "pyppyn" -Folder "C:\git\pyppyn\pyinstaller\dist" -KeyPrefix "20180321/1853_269fd71c5b96" -SearchPattern *.exe
             #Write-Host "9"
-            Get-ChildItem WSMan:\localhost\Shell
+            #Get-ChildItem WSMan:\localhost\Shell
 
-            $f = new-object System.IO.FileStream C:\Temp\test.dat, Create, ReadWrite
+            #$f = new-object System.IO.FileStream C:\Temp\test.dat, Create, ReadWrite
             #$f.SetLength(14.5MB) #worked
-            $f.SetLength(40MB) #worked
-            $f.Close()
-            Write-Host (Get-Date -UFormat "%Y/%m/%d %T")
-            Write-S3Object -BucketName $BucketName -Folder "C:\Temp" -KeyPrefix $KeyPrefix -SearchPattern "*.dat"
-            Write-Host "9"
+            #$f.SetLength(40MB) #worked
+            #$f.Close()
+            #Write-Host (Get-Date -UFormat "%Y/%m/%d %T")
+            #Write-S3Object -BucketName $BucketName -Folder "C:\Temp" -KeyPrefix $KeyPrefix -SearchPattern "*.dat"
+            #Write-Host "9"
 
             Write-Host (Get-Date -UFormat "%Y/%m/%d %T")
             Write-S3Object -BucketName $BucketName -Folder $Folder -KeyPrefix $KeyPrefix -SearchPattern "*.exe"
